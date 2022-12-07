e6afd7562e19513cdaa7318901f81c78cc6fca09
hlarrota <hlarrota.com>
2022-12-07 18:00:19 -0500

    first commit

---
+++ InstaYaGrupo6/src/components/ListaOrdenes.jsx
@@ -0,0 +1,72 @@
+import { useState, useEffect } from 'react';
+import '../App.css'
+import { NavbarOrdenes } from './NavbarOrdenes';
+import Axios from 'axios';
+import { useNavigate } from "react-router-dom";
+
+
+export function ListaOrdenes() {
+  const [listaOrdenes, setListaOrdenes] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(()=>
+    {
+      const url = "https://backend2-production-2904.up.railway.app/api/orders";
+      const headers = {
+        "x-access-token" : localStorage.getItem("accessToken"),
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      };
+      Axios.get(url,{headers: headers})
+      .then((res) =>{
+        debugger;
+        if (res.status === 200) {
+            setListaOrdenes(res.data);
+        }
+        else{
+          alert("Ocurrio un error");
+        }
+      });
+    },[setListaOrdenes]);
+
+    return (
+    <>
+      <NavbarOrdenes/>
+      <section className="bg-white rounded-bottom p-3">
+          <div className="container">
+              <h2 className="text-blue-dark text-center fs-2">Listado de órdenes</h2>
+              <div className="row justify-content-start my-4">
+                  <button className="btn btn-outline-primary w-auto"> <i className="bi bi-file-earmark-plus"></i> Ingresar nueva orden</button>
+              </div>
+              <div>
+                  <table className="table table-striped">
+                      <thead>
+                        <tr className='d-flex'>
+                          <th scope="col" className='col-2'># Servicio</th>
+                          <th scope="col" className='col-2'>Fecha</th>
+                          <th scope="col" className='col-2'>Ciudad entrega</th>
+                          <th scope="col" className='col-2'>Dirección entrega</th>
+                          <th scope="col" className='col-2'>Estado</th>
+                          <th scope="col" className='col-2'>Editar</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                      {listaOrdenes.map(
+                        (orden)=>(
+                          <tr className='d-flex'>
+                            <th scope="row" className='text-break col-2'>{orden._id}</th>
+                            <td className='col-2'>{orden.fecha.split('T')[0]}</td>
+                            <td className='col-2'>{orden.ciudadEntrega}</td>
+                            <td className='col-2'>{orden.direccionEntrega}</td>
+                            <td className='col-2'>{orden.estado}</td>
+                            <td className='col-2'><a href={"./actualizarOrdenes?id="+orden._id} className="btn btn-secondary"><i className="bi bi-pencil-square"></i> Editar</a></td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+              </div>
+          </div>
+      </section>
+    </>
+    );
+}
---
+++ InstaYaGrupo6/src/components/Login.jsx
@@ -0,0 +1,85 @@
+import '../App.css'
+import { NavbarLoginRegister } from './NavbarLoginRegister';
+import { useState } from 'react';
+import Axios from 'axios';
+import { useNavigate } from "react-router-dom";
+
+export function Login() {
+    const navigate = useNavigate();
+    const [loginInfo,setLoginInfo] = useState({
+        email: "",
+        password: ""
+    });
+
+    function handleChange(evt){
+        const { target } = evt;
+        const { name, value } = target;
+    
+        const newValues = {
+            ...loginInfo,
+            [name]: value, 
+        };
+    
+        setLoginInfo(newValues);
+    }
+
+    function handleSubmit(evt){
+        const url = "https://backend2-production-2904.up.railway.app/api/users/login";
+
+        const config = {
+            headers:{
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            }
+        };
+        
+        const data = {
+            email:loginInfo.email, 
+            password:loginInfo.password,
+        };
+
+        Axios.post(url,data,config)
+        .then((res) =>{
+            if (res.status === 200 && res.data.mensaje === 'Autenticación correcta') {
+                navigate("/listaOrdenes");
+                localStorage.setItem("accessToken", res.data.token);
+            }else{
+                alert(res.data.Error);
+            }
+        }).catch((err) =>{
+            debugger;
+            if(err.response.status === 401){
+                alert('El usuario o la contraseña son incorrectos');
+            }else{
+                console.log(err)
+            }});
+        evt.preventDefault();
+    }
+
+    return (
+    <>
+        <NavbarLoginRegister/>
+        <section className="bg-white rounded-bottom p-3">
+            <div className="container px-5">
+                <h2 className="text-blue-dark text-center fs-3">Iniciar sesión</h2>
+                <div className="text-center">
+                    <i className="bi bi-person-circle fs-icon-login"></i>
+                </div>
+                <form className="px-5" method='POST' onSubmit={handleSubmit}>
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputEmail1" className="form-label">Correo electronico</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name='email' value={loginInfo.email} onChange={handleChange} required/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputPassword1" className="form-label">Contraseña</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1" name='password' value={loginInfo.password} onChange={handleChange} required/>
+                    </div>
+                    <div className="row justify-content-center">
+                        <button type="submit" className="btn btn-primary w-50">Ingresar</button>
+                    </div>
+                </form>
+            </div>
+        </section>
+    </>
+    );
+}
